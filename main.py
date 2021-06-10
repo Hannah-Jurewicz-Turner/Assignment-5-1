f852ce1b05c4aaedbee7ab09a8f973fdf3dcbef0
HannahJT <84517064+Hannah-Jurewicz-Turner@users.noreply.github.com>
2021-06-10 01:33:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import math
+diameter = float(input("Enter the diameter of the circle:"))
+
+area = math.pi * diameter / 2 * diameter / 2
+
+print("Area of the circle is: {0}".format(area))
+
+import math
+diameter = float(input("Enter the diameter of the circle:"))
+
+area = math.pi * diameter
+
+print("Circumference of the circle is: {0}".format(area))
